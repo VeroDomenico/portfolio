e88005c8a2209f0565fd8b390f0a01ff5d2b9076
Dominic Meconi <meconidominic@gmail.com>
2023-09-22 17:43:14 -0500

    setup of landing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+
 import { Inter } from 'next/font/google'
 import Typewriter from '../components/Typewrite'
 
@@ -6,8 +6,11 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
-      <div className={"flex md:h-screen md:justify-center md:items-center"}>
+    <main className={`flex min-h-screen flex-col items-center justify-between pt-24 ${inter.className}`}>
+      <div className={"flex md:justify-center md:items-center flex-col"}>
+        <div className={""}>
+          <img className={"rounded-full border border-white-900 shadow-md rounded-full scale-75 "} src={"./headshot.png"} alt={"My Profile Picture"}/>
+        </div>
         <Typewriter messages={["Developer", "Designer", "Entrepreneur"]} />
       </div>
 

79531a4a39dab55e67f366e88e3964a8886aea45
Dominic Meconi <meconidominic@gmail.com>
2023-09-25 17:21:34 -0500

    card adjustments

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -4,15 +4,21 @@ type CardsProps = {
   str: string;
   desc: string;
   title: string;
+  link: string;
 };
 
-export default function Cards({ str, desc, title }: CardsProps): JSX.Element {
+export default function Cards({ str, desc, title, link }: CardsProps): JSX.Element {
+  // Helper function
+  const handleLinkClick = () => {
+    window.open(link, "_blank"); // Open the link in a new tab
+  };
+
   return (
-    <div className="relative w-64 h-64 px-2">
+    <div className="relative w-64 h-64 px-2" onClick={handleLinkClick}>
       <img
-        className="w-full h-full object-cover"
-        src={str} // Make sure the image path is correct
-        alt="Cat" // Provide a meaningful alt text for accessibility
+        className="w-full h-full object-fit"
+        src={str}
+        alt={title}
       />
       <div className="absolute top-0 left-0 w-full h-full bg-black opacity-0 hover:opacity-75">
         <hgroup className="flex justify-center items-center h-full text-white flex-col">
--- src/pages/projectsSkills.tsx
+++ src/pages/projectsSkills.tsx
@@ -8,8 +8,8 @@ export default function ProjectsSkills() {
         <div className={''}>
             <h1 className='flex justify-center'>My Project's</h1>
             <div className='flex flex-row justify-center py-6'>
-                <Cards str={"/OneImprint.jpg"} title={"OneImprint"} desc={"Full-Stack Development"} />
-                <Cards str={"/headshot.png"} title={"OneImprint"} desc={"Full-Stack Development"} />
+                <Cards str={"/OneImprint.jpg"} title={"OneImprint"} desc={"Full-Stack Development"} link={'https://www.oneimprint.com/'} />
+                <Cards str={"/VisaCCOLicense.png"} title={"Visa"} desc={"Engineer"} link={'https://www.visa.com'}/>
             </div>
         </div>
         );

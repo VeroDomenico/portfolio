858ab39099c337f0542684ba2d1994d5fd239888
Dominic Meconi <meconidominic@gmail.com>
2023-09-26 23:27:54 -0500

    broken?

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,31 @@
 import Typewriter from '../components/Typewrite';
 import AboutMe from '@/components/AboutMe';
 import ProjectsSkills from '@/pages/projectsSkills';
-import ContactMeFooter from '@/components/ContactMeFooter';
-import { Link as ScrollLink, Element } from 'react-scroll'; // Import ScrollLink and Element
+import ContactMeFooter from '@/components/ContactMeFooter'
 
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       {/* Profile Picture and Typewriter */}
-      <Element name="home">
-        <div className="flex flex-col items-center justify-center h-screen">
-          <div className="mb-4">
-            <img
-              className="rounded-full border border-white-900 shadow-md rounded-full scale-75"
-              src="./headshot.png"
-              alt="My Profile Picture"
-            />
-          </div>
-          <Typewriter messages={['Developer', 'Designer', 'Entrepreneur']} />
+      <div className="flex flex-col items-center justify-center h-screen">
+        <div className="mb-4">
+          <img
+            className="rounded-full border border-white-900 shadow-md rounded-full scale-75"
+            src="./headshot.png"
+            alt="My Profile Picture"
+          />
         </div>
-      </Element>
+        <Typewriter messages={['Developer', 'Designer', 'Entrepreneur']} />
+      </div>
 
       {/* About Me and Projects/Skills sections */}
-      <Element name="about">
-        <div className="bg-white text-black min-h-screen">
-          <AboutMe />
-        </div>
-      </Element>
-
-      <Element name="projectSkills" className='bg-white text-black'>
+      <div className="bg-gray-100 text-black">
+        <AboutMe />
         <ProjectsSkills />
-      </Element>
-
-      <ContactMeFooter />
+      </div>
+      <ContactMeFooter></ContactMeFooter>
     </div>
-  );
-}
+    );
+  }

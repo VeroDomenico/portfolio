583f7c9fe6aa42d5edbf46a9a2411db1f7aec9f5
Dominic Meconi <meconidominic@gmail.com>
2023-09-26 23:20:48 -0500

    broken?

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,8 @@ import { Inter } from 'next/font/google';
 import Typewriter from '../components/Typewrite';
 import AboutMe from '@/components/AboutMe';
 import ProjectsSkills from '@/pages/projectsSkills';
-import ContactMeFooter from '@/components/ContactMeFooter'
+import ContactMeFooter from '@/components/ContactMeFooter';
+import { Link as ScrollLink, Element } from 'react-scroll'; // Import ScrollLink and Element
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -10,26 +11,31 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       {/* Profile Picture and Typewriter */}
-      <div className="flex flex-col items-center justify-center h-screen">
-        <div className="mb-4">
-          <img
-            className="rounded-full border border-white-900 shadow-md rounded-full scale-75"
-            src="./headshot.png"
-            alt="My Profile Picture"
-          />
+      <Element name="home">
+        <div className="flex flex-col items-center justify-center h-screen">
+          <div className="mb-4">
+            <img
+              className="rounded-full border border-white-900 shadow-md rounded-full scale-75"
+              src="./headshot.png"
+              alt="My Profile Picture"
+            />
+          </div>
+          <Typewriter messages={['Developer', 'Designer', 'Entrepreneur']} />
         </div>
-        <Typewriter messages={['Developer', 'Designer', 'Entrepreneur']} />
-      </div>
+      </Element>
 
       {/* About Me and Projects/Skills sections */}
-      <div className="bg-gray-100 text-black">
-        <AboutMe />
-        <ProjectsSkills />
-      </div>
-      <ContactMeFooter></ContactMeFooter>
-      
-    </div>
-    
-    );
+      <Element name="about">
+        <div className="bg-white text-black min-h-screen">
+          <AboutMe />
+        </div>
+      </Element>
 
+      <Element name="projectSkills" className='bg-white text-black'>
+        <ProjectsSkills />
+      </Element>
+
+      <ContactMeFooter />
+    </div>
+  );
 }

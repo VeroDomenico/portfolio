cc20dbf62a2c2fe192527030ea19186238d303a1
Dominic Meconi <meconidominic@gmail.com>
2023-11-06 19:31:23 -0600

    date and time

--- src/pages/solarCalc.tsx
+++ src/pages/solarCalc.tsx
@@ -2,35 +2,35 @@ import suncalc from "suncalc";
 import { useState, useEffect } from "react";
 
 export default function solarCalc() {
-  const solarTime = suncalc.getTimes(new Date(), 30.2672, -97.7431);
-  const Waltz = new Date(solarTime.night);
-  Waltz.setHours(Waltz.getHours() + 2);
-
   const [location, setLocation] = useState({ lat: 0, lon: 0 });
+
   useEffect(() => {
     if ('geolocation' in navigator) {
-        navigator.geolocation.getCurrentPosition((position) => {
-            setLocation({
-            lat: position.coords.latitude,
-            lon: position.coords.longitude,
-            });
-        }, (error) => {
-            console.log(error);
+      navigator.geolocation.getCurrentPosition((position) => {
+        setLocation({
+          lat: position.coords.latitude,
+          lon: position.coords.longitude,
         });
+      }, (error) => {
+        console.log(error);
+      });
     } else {
-        console.log('Not Available');
+      console.log('Not Available');
     }
   }, [location]);
 
+  const solarTime = suncalc.getTimes(new Date(), location.lat, location.lon);
+  const Waltz = new Date(solarTime.night);
+  Waltz.setHours(Waltz.getHours() + 2);
 
   return (
-      <div className="flex flex-col bg-blue-200 text-black items-center justify-center h-screen">
-        <h2 className="mb-4">Dawn: {solarTime.dawn.toLocaleTimeString()}</h2>
-        <h2 className="mb-4">Sunrise: {solarTime.sunrise.toLocaleTimeString()}</h2>
-        <h2 className="mb-4">Lunch: {solarTime.solarNoon.toLocaleTimeString()}</h2>
-        <h2 className="mb-4">Sunset: {solarTime.sunset.toLocaleTimeString()}</h2>
-        <h2 className="mb-4">Night: {solarTime.night.toLocaleTimeString()}</h2>
-        <h2 className="mb-4">Waltz: {Waltz.toLocaleTimeString()}</h2>
-      </div>
+    <div className="flex flex-col bg-blue-200 text-black items-center justify-center h-screen">
+      <h2 className="mb-4">Dawn: {solarTime.dawn.toLocaleTimeString()}</h2>
+      <h2 className="mb-4">Sunrise: {solarTime.sunrise.toLocaleTimeString()}</h2>
+      <h2 className="mb-4">Lunch: {solarTime.solarNoon.toLocaleTimeString()}</h2>
+      <h2 className="mb-4">Sunset: {solarTime.sunset.toLocaleTimeString()}</h2>
+      <h2 className="mb-4">Night: {solarTime.night.toLocaleTimeString()}</h2>
+      <h2 className="mb-4">Waltz: {Waltz.toLocaleTimeString()}</h2>
+    </div>
   );
 }

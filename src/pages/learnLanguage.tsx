11c5a6e34ff5766da97814af326a1644660be3f3
Dominic Meconi <meconidominic@gmail.com>
2023-11-08 16:58:24 -0600

    language page

---
+++ src/pages/learnLanguage.tsx
@@ -0,0 +1,10 @@
+export default function learnLanguage() {
+
+
+
+    return (
+        <div className="flex flex-col bg-blue-200 text-black items-center justify-center h-screen">
+            <h2 className="mb-4">Learn Language</h2>
+        </div>
+    );
+}

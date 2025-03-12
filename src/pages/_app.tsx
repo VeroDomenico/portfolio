ed0bc1bbb586cd08ec050b2a7b433b272baa1d75
Dominic Meconi <domec@Dominics-MacBook-Pro.local>
2025-03-11 22:49:30 -0500

    updated site

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,12 +5,24 @@ import ContactMeFooter from '@/components/ContactMeFooter';
 import { ThemeProvider } from '../context/ThemeContext';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import { useState, useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <ThemeProvider>
-      <Navbar/>
-      <Component {...pageProps}/>
+      <Navbar isScrolled={isScrolled} />
+      <Component {...pageProps} />
     </ThemeProvider>
   );
 }

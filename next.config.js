aca08aadc1c575f382b11395147923076427c7bb
VeroDomenico <domecglobal@gmail.com>
2025-10-15 01:24:05 -0500

    PostHog Provider

--- next.config.js
+++ next.config.js
@@ -1,11 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
+  async rewrites() {
+    return [
+      {
+        source: '/ingest/static/:path*',
+        destination: 'https://us-assets.i.posthog.com/static/:path*',
+      },
+      {
+        source: '/ingest/:path*',
+        destination: 'https://us.i.posthog.com/:path*',
+      },
+    ]
+  },
+  webpack: {
+    resolve: {
+      modules: ['node_modules'],
+    },
+  },
 }
 
 module.exports = nextConfig
-module.exports = {
-  resolve: {
-    modules: ['node_modules'],
-  },
-};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,8 @@ import { ThemeProvider } from '../context/ThemeContext';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { useState, useEffect } from 'react';
+import posthog from 'posthog-js';
+import { PostHogProvider } from 'posthog-js/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -19,10 +21,22 @@ export default function App({ Component, pageProps }: AppProps) {
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+      api_host: '/ingest',
+      ui_host: 'https://us.posthog.com',
+      defaults: '2025-05-24',
+      capture_exceptions: true,
+      debug: process.env.NODE_ENV === 'development',
+    });
+  }, []);
+
   return (
-    <ThemeProvider>
-      <Navbar isScrolled={isScrolled} />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <PostHogProvider client={posthog}>
+      <ThemeProvider>
+        <Navbar isScrolled={isScrolled} />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </PostHogProvider>
   );
 }

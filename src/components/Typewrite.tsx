dd6afda097358564adf368d042f1195268a774be
VeroDomenico <domecglobal@gmail.com>
2025-10-15 20:19:57 -0500

    bugfix(typewriter)

--- src/components/Typewrite.tsx
+++ src/components/Typewrite.tsx
@@ -1,7 +1,5 @@
-// Import required dependencies
 import React, { useEffect, useState } from 'react';
 
-// Define TypewriterProps interface
 interface TypewriterProps {
   messages: string[];
   defaultWait?: number;
@@ -9,20 +7,17 @@ interface TypewriterProps {
   className?: string;
 }
 
-// Define Typewriter component
 const Typewriter: React.FC<TypewriterProps> = ({
   messages,
   defaultWait = 100,
   deleteWait = 50,
   className = '',
 }) => {
-  // Define state variables
   const [index, setIndex] = useState(0);
   const [subIndex, setSubIndex] = useState(0);
   const [backward, setBackward] = useState(false);
   const [blink, setBlink] = useState(true);
 
-  // Blink cursor effect
   useEffect(() => {
     const blinkInterval = setInterval(() => {
       setBlink((prev) => !prev);
@@ -30,7 +25,6 @@ const Typewriter: React.FC<TypewriterProps> = ({
     return () => clearInterval(blinkInterval);
   }, []);
 
-  // Typing effect
   useEffect(() => {
     if (!backward && subIndex === messages[index].length) {
       setTimeout(() => setBackward(true), 2000);
@@ -50,9 +44,8 @@ const Typewriter: React.FC<TypewriterProps> = ({
     return () => clearTimeout(timeout);
   }, [subIndex, index, backward, messages, defaultWait, deleteWait]);
 
-  // Render the typewriter component
   return (
-    <div className={`inline-flex items-center justify-center ${className}`}>
+    <div className={`inline-block items-center justify-center ${className}`}>
       <span className="mr-1">{messages[index].substring(0, subIndex)}</span>
       <span className={`font-light ${blink ? 'opacity-100' : 'opacity-0'} transition-opacity duration-100`}>
         |
